2e53130776a14d72be3677ba1aced5bf06c52def
gustavdias <gustav.almd@gmail.com>
2020-09-27 16:14:38 +0300

    add responsiveness

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,48 @@
-import React from 'react'
+
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+import { Grid } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import useStyles from "./Album/AlbumStyles";
 import Navbar from "./Navbar";
+import { Link } from 'react-router-dom';
 
 
 const Details = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Navbar/>
-            <h2>Окей Гугл!</h2>
-            <h2>Ok Google!</h2>
-        </div>
+
+        <>
+        <Navbar/>
+    <div className={classes.heroContent}>
+              <Container maxWidth="sm">
+                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Окей Гугл!
+                </Typography>
+                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Ok Google!
+                </Typography>
+                <div className={classes.heroButtons}>
+                  <Grid container spacing={2} justify="center">
+                    <Grid item>
+                     
+                    </Grid>
+                    <Grid item>
+                      <Button  component={Link}
+                        to="/app" variant="contained" color="primary">
+                        GO BACK
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+              </Container>
+              </div>
+    
+        </>
+
     )
 }
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,76 @@
 import React from "react";
 import auth from "../auth/auth";
+import Button from "@material-ui/core/Button";
 
-const LandingPage = props => {
+import { Grid } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import useStyles from "./Album/AlbumStyles";
+import Navbar from "./Navbar";
+
+const LandingPage = (props) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <h2>Please Login to access the app</h2>
-      <button
-        onClick={() => {
-          auth.login(() => {
-            props.history.push("/app");
-          });
-        }}
-      >
-        Login
-      </button>
-    </div>
+    <>
+      <Navbar />
+      <div className={classes.heroContent}>
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Landing Page
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Целевая страница
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+          >
+            Please Login to access the app
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+          >
+            Пожалуйста, войдите, чтобы получить доступ к приложению
+          </Typography>
+          <div className={classes.heroButtons}>
+            <Grid container spacing={2} justify="center">
+              <Grid item></Grid>
+              <Grid item>
+                <Button
+                  onClick={() => {
+                    auth.login(() => {
+                      props.history.push("/app");
+                    });
+                  }}
+                  variant="contained"
+                  color="primary"
+                >
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 };
 export default LandingPage;
